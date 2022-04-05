07adf30271ebf13c2b3fbb43058d2a6eefab0ace
Darya Volkova <anrich@bk.ru>
2022-04-05 18:13:38 +0300

    Added button component. Added style for component.

--- src/components/GameButton.vue
+++ src/components/GameButton.vue
@@ -14,3 +14,26 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+button {
+  position: relative;
+  border-radius: 6px;
+  border: none;
+  padding: 30px;
+  margin: 4px;
+
+  &:hover {
+    &:after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0px;
+      left: 0px;
+      background: rgba(0, 0, 0, 0.123);
+      border-radius: inherit;
+    }
+  }
+}
+</style>
